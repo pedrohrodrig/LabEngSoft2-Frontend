6051a94fb74b0e1806d425ce39fd0979b210f274
lucas-pvg <lucaspvg1@gmail.com>
2024-03-20 16:03:16 -0300

    ✨ Added JS objects for simulation

--- frontend/src/objects/profile.js
+++ frontend/src/objects/profile.js
@@ -1,21 +1,20 @@
-const professional = [
-    {
+const professional = {
         id: 1,
         name: "Lucas Garieri",
         photo: "/images/user.jpeg",
-        phone: "61982653119",
+        phone: "(61) 98265-3119",
         age: 22,
         service: "doctor",
         appointment: "online",
         email: "lucas_pvg@usp.br",
+        address: "Rua Boturoca 220",
         price: "100,00",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis dui dignissim, rhoncus felis a, aliquam tellus. Pellentesque ultrices egestas ipsum, sed convallis tortor dignissim luctus. Etiam hendrerit enim in eleifend consectetur. Lorem ipsum dolor sit amet",
         appointments: [1, 2, 3, 4, 5],
         patients: [1, 2, 3, 4, 5]
-    }
-];
+};
 
-const patient = [
-    {
+const patient = {
         id: 1,
         name: "Lucas Garieri",
         photo: "/images/user.jpeg",
@@ -26,7 +25,6 @@ const patient = [
         email: "lucas_pvg@usp.br",
         appointments: [1, 6, 7, 8],
         professionals: [1, 2, 3, 4]
-    }
-];
+};
 
 export { professional, patient };
