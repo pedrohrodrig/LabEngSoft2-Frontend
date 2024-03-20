4e16a3411236ad383ac51e4781de44d962d36c51
lucas-pvg <lucaspvg1@gmail.com>
2024-03-20 14:55:01 -0300

    ✨ Add Button

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -4,12 +4,12 @@ import Icon from "../icon/icon";
 
 import "./button.css";
 
-function Button({ className, to, text, icon, iconType }) {
+function Button({ className, to, text, icon, iconType, onClick }) {
     const classes = icon ? `but ${icon} ${className}` : `but text ${className}`;
 
     return (
-        <div className={classes}>
-            <Link to={to}>
+        <div className={classes} onClick={onClick}>
+            <Link className="but-link" to={to}>
                 <Icon iconType={iconType} />
                 {text}
             </Link>
