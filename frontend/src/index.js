e5241826a865f859e46bcb6643faf5a07eb98507
lucas-pvg <lucaspvg1@gmail.com>
2024-03-20 16:10:08 -0300

    🚀 Include paths to nem pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import LayoutPage from './pages/layoutpage/layoutpage';
-import OverviewPage from './pages/professional/overviewpage/overviewpage';
-
-import PatientsPage from './pages/patientspage/patientspage';
-import AppointmentsPage from './pages/appointmentspage/appointmentspage';
-import PaymentsPage from './pages/paymentspage/paymentspage';
-import TestPage from './pages/testpage/testpage';
 import App from './App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />}>
-
-                    <Route index element={<LayoutPage />} />
-                    <Route path="overview" element={<OverviewPage />} />
-
-                    <Route path="patients" element={<PatientsPage />} />
-                    <Route path="appointments" element={<AppointmentsPage />} />
-                    <Route path="payments" element={<PaymentsPage />} />
-
-                    <Route path="test" element={<TestPage />} />
-                
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <App />
     </React.StrictMode>
 );
