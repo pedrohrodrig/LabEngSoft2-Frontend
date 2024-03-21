e95ce1d14fa91dd21c80618d220e23fd6de07202
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-20 23:13:59 -0300

    Atualiza página de cadastro

--- frontend/src/pages/signUpPage/signUpPage.js
+++ frontend/src/pages/signUpPage/signUpPage.js
@@ -12,64 +12,58 @@ function SignUpPage() {
           body="Todos os serviços de saúde em um só lugar"
         />
         {/* TODO: adicionar validação do Forms via Formik (?)*/}
-        <Form style={{width: "100%"}}>
+        <Form className="pl-3 pr-3" style={{width: "100%", "overflow-y": "scroll", "overflow-x": "hidden"}}>
           <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formName">
+              <Form.Group as={Col} className="mb-3" controlId="formName">
                 <Form.Label>Nome Completo</Form.Label>
                 <Form.Control placeholder="Digite seu nome" />
               </Form.Group>
-            </Col>
           </Row>
           <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formMobilePhone">
+              <Form.Group as={Col} className="mb-3" controlId="formMobilePhone">
                 <Form.Label>Telefone</Form.Label>
                 <Form.Control type="number" placeholder="11999999999" />
               </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formBirthDate">
+              <Form.Group as={Col} className="mb-3" controlId="formBirthDate">
                 <Form.Label>Data de Nascimento</Form.Label>
                 <Form.Control type="date" />
               </Form.Group>
-            </Col>
           </Row>
           <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formHeight">
+              <Form.Group as={Col} className="mb-3" controlId="formCPF">
+                <Form.Label>CPF</Form.Label>
+                <Form.Control type="number" placeholder="Apenas números" />
+              </Form.Group>
+              <Form.Group as={Col} className="mb-3" controlId="formRG">
+                <Form.Label>RG</Form.Label>
+                <Form.Control type="number" placeholder="Apenas números" />
+              </Form.Group>
+          </Row>
+          <Row>
+              <Form.Group as={Col} className="mb-3" controlId="formHeight">
                 <Form.Label>Altura</Form.Label>
                 <Form.Control type="number" placeholder="Em metros" />
               </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formWeight">
+              <Form.Group as={Col} className="mb-3" controlId="formWeight">
                 <Form.Label>Peso</Form.Label>
                 <Form.Control type="number" placeholder="Em kg" />
               </Form.Group>
-            </Col>
           </Row>
           <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Group as={Col} className="mb-3" controlId="formEmail">
                 <Form.Label>E-mail</Form.Label>
                 <Form.Control type="email" placeholder="Digite seu e-mail" />
               </Form.Group>
-            </Col>
           </Row>
           <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Group as={Col} className="mb-3" controlId="formPassword">
                 <Form.Label>Senha</Form.Label>
                 <Form.Control type="password" placeholder="Digite sua senha" />
               </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formConfirmPassword">
+              <Form.Group as={Col} className="mb-3" controlId="formConfirmPassword">
                 <Form.Label>Confirmar senha</Form.Label>
                 <Form.Control type="password" placeholder="Confirme sua senha" />
               </Form.Group>
-            </Col>
           </Row>
           <Form.Group className="mb-3" controlId="formCheckbox">
             <Form.Check type="checkbox" label="Concordo com os termos e condições" />
