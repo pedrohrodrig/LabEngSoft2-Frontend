7d5d7b0fe1f640d87755837450ed35ca3dd0d229
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-03-21 00:40:59 -0300

    Permite que os cards sejam clicáveis na tela de histórico

---
+++ frontend/src/pages/patient/patientHistoryDetailPage/appointdetailpage.js
@@ -0,0 +1,73 @@
+import React, { useContext, useState } from "react";
+import Title from "../../../components/title/title";
+import Textbox from "../../../components/textbox/textbox";
+import HorizontalCard from "../../../components/horizontalcard/horizontalcard";
+import DocumentList from "../../../components/lists/documentlist/documentlist";
+import Button from "../../../components/button/button";
+import Popup from "../../../components/popup/popup";
+
+import documentList from "../../../objects/documents";
+import appointmentList from "../../../objects/appointments";
+import UserContext from "../../../contexts/UserContext";
+import "./appointdetailpage.css";
+
+function PatientHistoryDetailPage() {
+  const { user } = useContext(UserContext);
+  const [ avaliation, setAvaliation ] = useState(false);
+  const [ cancelation, setCancelation ] = useState(false);
+  const appointment = appointmentList[0];
+
+  return (
+    <div className="appoint-detail page">
+      <div className="header">
+        <Title head="Detalhes da Consulta" />
+      </div>
+
+      <div className="content">
+        <div className="profile-text">
+          <div className="cardbox">
+            <Textbox title="Profissional" />
+            <HorizontalCard className="small" image={user.photo} title={user.name} to="/professional/1" />
+          </div>
+          <Textbox className="horizontal" title="Status" body={appointment.status} />
+          <Textbox className="horizontal" title="Tipo" body={appointment.type} />
+          <Textbox className="horizontal" title="Horário" body={appointment.timestamp} />
+          <Textbox className="horizontal" title="Endereço" body={appointment.address} />
+
+          <div className="appoint-but">
+            <Button className="grad medium" text="Avaliar" icon="left" iconType="clipboard" onClick={() => setAvaliation(true)} />
+            <Button className="black medium outline" text="Cancelar" icon="left" iconType="x" onClick={() => setCancelation(true)} />
+          </div>
+
+        </div>
+
+        <div className="extra">
+          <div className="doc-header">
+            <h1>Documentos</h1>
+          </div>
+          <DocumentList className="patient-docs" small={true} documents={documentList} />
+        </div>
+      </div>
+
+      <Popup
+        isOpen={avaliation}
+        closePopup={() => setAvaliation(false)}
+        head="Avaliação"
+        body="Selecione o tipo de documento:"
+        buttons={["Avaliação", "Treinamento"]}
+        iconType="clipboard"
+      />
+
+      <Popup
+        isOpen={cancelation}
+        closePopup={() => setCancelation(false)}
+        head="Cancelamento"
+        body="Deseja mesmo cancelar essa consulta?"
+        buttons={["Manter", "Cancelar"]}
+        iconType="x"
+      />
+    </div>
+  );
+}
+
+export default PatientHistoryDetailPage;
