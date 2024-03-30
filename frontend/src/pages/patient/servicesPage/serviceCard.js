1d728d18f6e1c64c0f59833f4d8e60669d745974
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2024-03-29 23:07:55 -0300

    ajustando página de serviços

--- frontend/src/pages/patient/servicesPage/serviceCard.js
+++ frontend/src/pages/patient/servicesPage/serviceCard.js
@@ -4,17 +4,20 @@ import Button from "react-bootstrap/Button";
 import ApolloLogo from "../../../assets/apollo-logo.svg"
 import { Link } from "react-router-dom";
 
-function ServiceCard({title, description}) {
+import './serviceCard.css'
+
+function ServiceCard({ title, description }) {
   return (
     <Card style={{ width: "20rem" }}>
       <Card.Body>
-        <Card.Img variant="top" src={ApolloLogo} style={{width: "30%"}} className="mb-3"/>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text className="text-muted fs-6">
+        <Card.Img variant="top" src={ApolloLogo} style={{ width: "30%", margin: '0.2rem' }} className="mb-3" />
+        <Card.Title style={{ fontWeight: 'bold', margin: '0.2rem' }}>{title}</Card.Title>
+        <hr className="divider" />
+        <p className="description">
           {description}
-        </Card.Text>
-        <Link to="../professionals" state={{ title }}  className="d-flex justify-content-end mt-3 text-decoration-none">
-          <Button variant="secondary">Busque aqui!</Button>
+        </p>
+        <Link to="../professionals" state={{ title }} className="d-flex justify-content-end mt-3 text-decoration-none">
+          <Button variant="secondary" className="select">Buscar!</Button>
         </Link>
       </Card.Body>
     </Card>
