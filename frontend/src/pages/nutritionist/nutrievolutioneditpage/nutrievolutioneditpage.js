3b63cfe2a553d6bb6b63a5fb35d364422d0ec01d
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 11:55:57 -0300

    ✨ Add nutrititionist evolution edit page

---
+++ frontend/src/pages/nutritionist/nutrievolutioneditpage/nutrievolutioneditpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutrievolutioneditpage.css";
+
+function NutriEvolutionEditPage() {
+  return (
+    <div className="evolution-edit page">
+      <Title head="Editar Evolução" body="Edite a evolução do paciente" />
+    </div>
+  );
+}
+
+export default NutriEvolutionEditPage;
