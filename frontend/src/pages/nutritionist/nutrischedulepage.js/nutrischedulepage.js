15c96aa64f80f4ddc0516d1fb17ef7dff8a04eaa
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 12:24:57 -0300

    ✨ Add nutritionist schedule page

---
+++ frontend/src/pages/nutritionist/nutrischedulepage.js/nutrischedulepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutrischedulepage.css";
+
+function NutriSchedulePage() {
+  return (
+    <div className="schedule page">
+      <Title head="Agenda" body="Veja sua agenda de consultas" />
+    </div>
+  );
+}
+
+export default NutriSchedulePage;
