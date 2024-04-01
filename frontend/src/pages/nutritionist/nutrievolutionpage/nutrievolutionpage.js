92443e6e532971382f08a14cc6a69898e0ba5160
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 11:56:07 -0300

    ✨ Add nutrititionist evolution page

---
+++ frontend/src/pages/nutritionist/nutrievolutionpage/nutrievolutionpage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutrievolutionpage.css";
+
+function NutriEvolutionPage() {
+  return (
+    <div className="evolution page">
+      <Title
+        head="Evolução do Paciente"
+        body="Veja a evolução do paciente"
+      />
+    </div>
+  );
+}
+
+export default NutriEvolutionPage;
