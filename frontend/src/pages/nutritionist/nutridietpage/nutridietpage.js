598f87ef761eaf60d593623bb1b2274797b0c520
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 11:54:52 -0300

    ✨ Add nutrititionist diet page

---
+++ frontend/src/pages/nutritionist/nutridietpage/nutridietpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutridietpage.css";
+
+function NutriDietPage() {
+  return (
+    <div className="diet page">
+      <Title head="Dieta" body="Confira a dieta do paciente" />
+    </div>
+  );
+}
+
+export default NutriDietPage;
