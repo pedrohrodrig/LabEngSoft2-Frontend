b72fb072954b80d02c13d12ba76b12c23ad9358c
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 11:57:10 -0300

    ✨ Add nutritionist profile edit page

---
+++ frontend/src/pages/nutritionist/nutriprofileeditpage/nutriprofileeditpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutriprofileeditpage.css";
+
+function NutriProfileEditPage() {
+  return (
+    <div className="profile-edit page">
+      <Title head="Editar Perfil" body="Edite o seu perfil" />
+    </div>
+  );
+}
+
+export default NutriProfileEditPage;
