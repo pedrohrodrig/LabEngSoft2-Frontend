23f8c96cb29572fc708571c9fb9551a5cf9d3b80
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 11:55:04 -0300

    ✨ Add nutrititionist evaluation edit page

---
+++ frontend/src/pages/nutritionist/nutrievaluationeditpage/nutrievaluationeditpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../../../components/title/title";
+
+import "./nutrievaluationeditpage.css";
+
+function NutriEvaluationEditPage() {
+  return (
+    <div className="evaluation-edit page">
+      <Title head="Editar Avaliação" body="Edita a avaliação do paciente" />
+    </div>
+  );
+}
+
+export default NutriEvaluationEditPage;
