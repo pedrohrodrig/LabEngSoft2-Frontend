068bba7aee34acee2992d6b052d15478c64af728
asakuraLie <lie.asakura000@gmail.com>
2024-04-01 22:00:02 -0300

    🤡 Mock meals

---
+++ frontend/src/objects/meals.js
@@ -0,0 +1,24 @@
+const mealList = [
+  {
+    id: 1,
+    food: "Pão",
+    quantity: "3",
+  },
+  {
+    id: 2,
+    food: "Pão",
+    quantity: "2",
+  },
+  {
+    id: 3,
+    food: "Pão",
+    quantity: "6",
+  },
+  {
+    id: 4,
+    food: "Salsicha",
+    quantity: "5",
+  },
+];
+
+export default mealList;
