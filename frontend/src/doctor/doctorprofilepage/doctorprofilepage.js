62835d2f8ff7590ba41c11d092e942a0e890971e
VictorASantana <victor.asantana012@gmail.com>
2024-04-02 20:47:21 -0300

    creating doctor profile page

--- frontend/src/doctor/doctorprofilepage/doctorprofilepage.js
+++ frontend/src/doctor/doctorprofilepage/doctorprofilepage.js
@@ -12,9 +12,7 @@ function DoctorProfilePage() {
     return (
         <div className="profile page">
             <div className="header">
-
                 <Profile name={user.name} info={`R$${user.price} p/ consulta`} edit={true} to="/doctor-home" />
-
             </div>
 
             <div className="content">
