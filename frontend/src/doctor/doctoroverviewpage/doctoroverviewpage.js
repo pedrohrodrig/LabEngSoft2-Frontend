ff85cfd4d5f3172f41244e8d051f996d8f9842e1
VictorASantana <victor.asantana012@gmail.com>
2024-04-02 22:20:49 -0300

    adjusting doctor overview page

--- frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
+++ frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
@@ -5,9 +5,13 @@ import BigNumber from "../../components/bignumber/bignumber";
 import appointmentList from "../../objects/appointments";
 import AppointmentList from "../../components/lists/appointmentlist/appointmentlist";
 import UserContext from "../../contexts/UserContext";
+import patientList from "../../objects/patients";
+import paymentsList from "../../objects/payments";
 
 function DoctorOverviewPage() {
   const { user } = useContext(UserContext);
+  let amount = 0
+  paymentsList.forEach((element) => element.status === "Pago" ? amount += element.value : amount += 0);
   const appointments = appointmentList.filter((appoint) => {
     return user.appointments.includes(appoint.id);
   });
@@ -15,15 +19,15 @@ function DoctorOverviewPage() {
   return (
     <div className="overview page">
       <div className="bignumbers">
-        <BigNumber number="1k" text="Consultas" iconType="activity" to="/doctor-appointments" />
+        <BigNumber number={appointmentList.length} text="Consultas" iconType="activity" to="/doctor-appointments" />
         <BigNumber
-          number="40"
+          number={patientList.length}
           text="Pacientes"
           iconType="user"
           to="/doctor-patients"
         />
         <BigNumber
-          number="R$200"
+          number={`R$ ${amount}`}
           text="Renda Mensal"
           iconType="dollar"
           to="/doctor-payments"
@@ -31,7 +35,6 @@ function DoctorOverviewPage() {
       </div>
 
       <div className="appoint">
-        <h1 className="appoint-title">Consultas Hoje</h1>
         <AppointmentList appointments={appointments} />
       </div>
     </div>
