5b548089a05bb216cb5dda48e99989891577d142
VictorASantana <victor.asantana012@gmail.com>
2024-04-04 16:57:58 -0300

    adjusting doctor patient pages

--- frontend/src/doctor/doctorpatientlistpage/doctorpatientlistpage.js
+++ frontend/src/doctor/doctorpatientlistpage/doctorpatientlistpage.js
@@ -48,6 +48,7 @@ function DoctorPatientListPage() {
         body="Confira os pacientes que se consultam com você"
       />
       <PatientList patients={patients} to={"/doctor-patient-profile?"}/>
+      <PatientList patients={patients} to={"/doctor-patient-profile?"}/>
 
     </div>
   );
