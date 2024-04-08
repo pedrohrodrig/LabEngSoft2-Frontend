c02bfc85ee7c1be49a82d10463b8023937c7586e
asakuraLie <lie.asakura000@gmail.com>
2024-04-08 18:15:53 -0300

    ⚡️ Responsive edit diet page with dummy diet list

--- frontend/src/pages/nutritionist/nutridieteditpage/nutridieteditpage.js
+++ frontend/src/pages/nutritionist/nutridieteditpage/nutridieteditpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import Title from "../../../components/title/title";
 import Button from "../../../components/button/button";
 import EditMealList from "../../../components/lists/editmeallist/editmeallist";
@@ -8,56 +8,85 @@ import mealList from "../../../objects/meals";
 import "./nutridieteditpage.css";
 
 function NutriDietEditPage() {
+  const [weekday, setWeekday] = useState("monday");
+
+  function handleClick(weekday) {
+    setWeekday(weekday);
+  }
+
   return (
     <div className="diet page">
       <div className="header">
         <Title head="Editar Plano Alimentar" />
       </div>
       <div className="button">
-        <Button className="grad small" text="Salvar" to="/nutritionist/diet" />
+        <Button
+          className="grad small"
+          text="Salvar"
+          to="/nutritionist/diet"
+        />
       </div>
       <div className="separation">
         <div className="days">
-          <Button className="grad small" text="Segunda" />
-          <Button className="grad small" text="Terça" />
-          <Button className="grad small" text="Quarta" />
-          <Button className="grad small" text="Quinta" />
-          <Button className="grad small" text="Sexta" />
-          <Button className="grad small" text="Sábado" />
-          <Button className="grad small" text="Domingo" />
+          <Button
+            className="grad small"
+            text="Segunda"
+            onClick={() => handleClick("monday")}
+          />
+          <Button
+            className="grad small"
+            text="Terça"
+            onClick={() => handleClick("tuesday")}
+          />
+          <Button
+            className="grad small"
+            text="Quarta"
+            onClick={() => handleClick("wednesday")}
+          />
+          <Button
+            className="grad small"
+            text="Quinta"
+            onClick={() => handleClick("thursday")}
+          />
+          <Button
+            className="grad small"
+            text="Sexta"
+            onClick={() => handleClick("friday")}
+          />
+          <Button
+            className="grad small"
+            text="Sábado"
+            onClick={() => handleClick("saturday")}
+          />
+          <Button
+            className="grad small"
+            text="Domingo"
+            onClick={() => handleClick("sunday")}
+          />
         </div>
         <div className="diet-content">
           <div className="diet-text">
-            <div className="meal">
-              <h2>Desjejum</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
-            <div className="meal">
-              <h2>Lanche da manhã</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
-            <div className="meal">
-              <h2>Almoço</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
-            <div className="meal">
-              <h2>Lanche da Tarde</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
-            <div className="meal">
-              <h2>Jantar</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
-            <div className="meal">
-              <h2>Ceia</h2>
-              <Button className="grad outline small" text="Adicionar" />
-            </div>
-            <EditMealList meals={mealList} />
+            {weekday === "monday" && (
+              <EditMealList meals={mealList[0].monday} />
+            )}
+            {weekday === "tuesday" && (
+              <EditMealList meals={mealList[0].tuesday} />
+            )}
+            {weekday === "wednesday" && (
+              <EditMealList meals={mealList[0].wednesday} />
+            )}
+            {weekday === "thursday" && (
+              <EditMealList meals={mealList[0].thursday} />
+            )}
+            {weekday === "friday" && (
+              <EditMealList meals={mealList[0].friday} />
+            )}
+            {weekday === "saturday" && (
+              <EditMealList meals={mealList[0].saturday} />
+            )}
+            {weekday === "sunday" && (
+              <EditMealList meals={mealList[0].sunday} />
+            )}
           </div>
         </div>
       </div>
