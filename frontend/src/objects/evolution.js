4b8ae0af9c3fe6a679361b1e08068b205fd49182
asakuraLie <lie.asakura000@gmail.com>
2024-04-08 14:50:00 -0300

    🤡 Evolution list

---
+++ frontend/src/objects/evolution.js
@@ -0,0 +1,30 @@
+const evolutionList = [
+  {
+    id: 1,
+    patient: "Lucas",
+    date: "27/04",
+    weight: "60kg",
+    imc: "20",
+    activity: "4x na semana",
+    appetite: "Normal",
+    chewing: "Rápido",
+    intestine: "Normal",
+    sleep: "Melhorou desde a última consulta",
+    comments: "Nada a declarar",
+  },
+  {
+    id: 2,
+    patient: "Lucas",
+    date: "20/05",
+    weight: "62kg",
+    imc: "21",
+    activity: "5x na semana",
+    appetite: "Normal",
+    chewing: "Normal",
+    intestine: "Funcionando bem melhor",
+    sleep: "Melhor que nunca",
+    comments: "Ainda nada a declarar",
+  },
+];
+
+export default evolutionList;
