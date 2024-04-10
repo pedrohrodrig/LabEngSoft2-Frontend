043c033da1c41f0fdc756bca53a71768c345402d
IsaacYusuke <isaacyusuke@usp.br>
2024-04-10 16:22:02 -0300

    Objetos psychologist e consultations

---
+++ frontend/src/objects/consultations.js
@@ -0,0 +1,39 @@
+const consultations = [
+    {
+        psychologist: 1, // Assumindo que este é o ID do psicólogo
+        id_patient: 1,
+        status: "Requested",
+        scheduled_time: "2024-03-18T18:00:00",
+        payment_proof: null, // Nenhum comprovante de pagamento para consultas solicitadas
+    },
+    {
+        psychologist: 1,
+        id_patient: 2,
+        status: "Awaiting Payment",
+        scheduled_time: "2024-03-18T19:00:00",
+        payment_proof: null, // Assumindo que está aguardando pagamento
+    },
+    {
+        psychologist: 1,
+        id_patient: 3,
+        status: "Confirmed",
+        scheduled_time: "2024-03-18T20:00:00",
+        payment_proof: "/payment_proofs/payment_proof_3.pdf", // Exemplo de comprovante de pagamento
+    },
+    {
+        psychologist: 1,
+        id_patient: 4,
+        status: "Completed",
+        scheduled_time: "2024-03-19T18:00:00",
+        payment_proof: "/payment_proofs/payment_proof_4.pdf", // Exemplo de comprovante de pagamento
+    },
+    {
+        psychologist: 1,
+        id_patient: 5,
+        status: "Cancelled",
+        scheduled_time: "2024-03-20T18:00:00",
+        payment_proof: null, // Nenhum comprovante de pagamento para consultas canceladas
+    },
+];
+
+export default consultations;
---
+++ frontend/src/objects/psychologists.js
@@ -0,0 +1,18 @@
+const psychologists = [
+    {
+        id_user: 1,
+        full_name: "Dr. Alex Mendes",
+        date_of_birth: "1980-04-15",
+        email: "alex.mendes@example.com",
+        phone_number: "(11) 99999-9999",
+        document: "123.456.789-00",
+        start_time_morning: "08:00",
+        end_time_morning: "12:00",
+        start_time_afternoon: "14:00",
+        end_time_afternoon: "18:00",
+        consultation_price: 150.00,
+        profile_picture: "/profile_pictures/alex_mendes_profile.jpg"
+    }
+];
+
+export default psychologists;
