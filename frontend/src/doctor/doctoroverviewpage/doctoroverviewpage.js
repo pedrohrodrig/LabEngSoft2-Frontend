67f1e3a45d9424256a37da7879f1c0c9bc95a7d9
VictorASantana <victor.asantana012@gmail.com>
2024-04-11 18:57:10 -0300

    adjusting overview page to show today appointments

--- frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
+++ frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import "./doctoroverviewpage.css";
 import BigNumber from "../../components/bignumber/bignumber";
@@ -7,14 +7,77 @@ import AppointmentList from "../../components/lists/appointmentlist/appointmentl
 import UserContext from "../../contexts/UserContext";
 import patientList from "../../objects/patients";
 import paymentsList from "../../objects/payments";
+import axios from "axios";
+import { format, isToday, parseISO } from 'date-fns';
 
 function DoctorOverviewPage() {
   const { user } = useContext(UserContext);
   let amount = 0
   paymentsList.forEach((element) => element.status === "Pago" ? amount += element.value : amount += 0);
-  const appointments = appointmentList.filter((appoint) => {
-    return user.appointments.includes(appoint.id);
-  });
+  const [appointments, setAppointments] = useState([])
+
+    const formatDate = (date) => {
+        return format(parseISO(date), "dd MMM yyyy ' - ' HH:mm");
+    }
+
+    const appointmentStatus = (status) => {
+        if (status === 1) return "Confirmada";
+        else if (status === 2) return "Finalizada";
+        else return "Cancelada"
+    }
+
+    const verifyToday = (date) => {
+      const today = new Date();
+      const dateToVerify = new Date(date);
+      return today.getFullYear() === dateToVerify.getFullYear() &&
+             today.getMonth() === dateToVerify.getMonth() &&
+             today.getDate() === dateToVerify.getDate();
+    }
+
+    const verifyAppointment = (newAppointment) => {
+        for (const element of appointments) {
+          if (element.patientid !== newAppointment.patientid) return true
+          if (element.timestamp !== newAppointment.timestamp) return true
+        }
+        return false;
+    }
+
+    const createAppointment = (newAppointment) => {
+        return {
+            id: newAppointment.id,
+            patient: newAppointment.patient.first_name + " " + newAppointment.patient.last_name,
+            patientid: newAppointment.patient.id_user,
+            phone: newAppointment.patient.phone,
+            photo: "/images/user.jpeg",
+            professional: user.name,
+            professionalid: newAppointment.id_user_professional,
+            date: "",
+            hour: "",
+            timestamp: formatDate(newAppointment.datetime),
+            status: appointmentStatus(newAppointment.status),
+            type: "Presencial",
+            address: user.address
+        }
+    }
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/appointment/`)
+            .then(response => {
+              for (const element of response.data) {
+                  const newAppointment = createAppointment(element);
+                  if (verifyToday(element.datetime) && newAppointment.status === "Confirmada") {
+                    if (appointments.length === 0) setAppointments([newAppointment])
+                    if (verifyAppointment(newAppointment)) {
+                        setAppointments(prev => [...prev, newAppointment]);
+                    }
+                }
+              }
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }, []);
+
 
   return (
     <div className="overview page">
@@ -36,7 +99,7 @@ function DoctorOverviewPage() {
 
       <div className="appoint">
         <h1 className="appoint-title">Consultas Hoje</h1>
-        <AppointmentList appointments={appointments} />
+        <AppointmentList appointments={appointments} to={"/doctor-appointments-detail?"} />
       </div>
     </div>
   );
