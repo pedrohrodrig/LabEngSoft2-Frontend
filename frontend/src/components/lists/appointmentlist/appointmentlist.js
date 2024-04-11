02ce4348b545cab70f0693c773213d0c08c48425
VictorASantana <victor.asantana012@gmail.com>
2024-04-11 18:13:40 -0300

    adjusting navigation to pass appointment id

--- frontend/src/components/lists/appointmentlist/appointmentlist.js
+++ frontend/src/components/lists/appointmentlist/appointmentlist.js
@@ -4,7 +4,6 @@ import PathContext from "../../../contexts/PathContext";
 
 import "../cardlist.css";
 
-
 function AppointmentList({ className, appointments, small, to}) {
     const classes = small ? `small-card-list ${className}` : `card-list ${className}`;
     const n = small ? 3 : appointments.length;
