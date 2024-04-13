70cff590791cdd0708754883753c1e2b0a737c68
VictorASantana <victor.asantana012@gmail.com>
2024-04-13 18:45:13 -0300

    adjusting doctor patient pages

--- frontend/src/doctor/doctorpatientlistpage/doctorpatientlistpage.js
+++ frontend/src/doctor/doctorpatientlistpage/doctorpatientlistpage.js
@@ -1,16 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import "./doctorpatientlistpage.css";
 import UserContext from "../../contexts/UserContext";
-import patientList from "../../objects/patients";
 import Title from "../../components/title/title";
 import PatientList from "../../components/lists/patientlist/patientlist";
+import axios from "axios";
 
 function DoctorPatientListPage() {
   const { user } = useContext(UserContext);
-  const patients = patientList.filter((patient) => {
-    return user.patients.includes(patient.id);
-  });
+  const [patients, setPatients] = useState([]);
+
+  const filterPatient = (patients) => {
+    const conjunto = new Set(patients.map(JSON.stringify));
+    const vetorSemDuplicatas = Array.from(conjunto).map(JSON.parse);
+    return vetorSemDuplicatas;
+  }
+
+  const createPatient = (newData) => {
+    return {
+        id: newData.patient.id_user,
+        name: newData.patient.first_name + " " + newData.patient.last_name,
+        photo: "/images/user.jpeg",
+        phone: newData.patient.phone,
+        age: 22,
+        height: 170,
+        weight: 65,
+        email: newData.patient.email
+    }
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/appointment_from_professional_id/${user.id}`)
+        .then(response => {
+          const patientList = response.data.map(element => createPatient(element));
+          const patientFilter = filterPatient(patientList);
+          setPatients(patientFilter);
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }, []);
 
   return (
     <div className="patients page">
