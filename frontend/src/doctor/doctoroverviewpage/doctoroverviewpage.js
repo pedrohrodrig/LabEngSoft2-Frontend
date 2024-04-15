8b179b35cfa88d16161e8d7d35a7bd25cec7e563
VictorASantana <victor.asantana012@gmail.com>
2024-04-15 11:45:32 -0300

    adjusting overview page to display the actual number of patients

--- frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
+++ frontend/src/doctor/doctoroverviewpage/doctoroverviewpage.js
@@ -2,10 +2,8 @@ import React, { useContext, useEffect, useState } from "react";
 
 import "./doctoroverviewpage.css";
 import BigNumber from "../../components/bignumber/bignumber";
-import appointmentList from "../../objects/appointments";
 import AppointmentList from "../../components/lists/appointmentlist/appointmentlist";
 import UserContext from "../../contexts/UserContext";
-import patientList from "../../objects/patients";
 import paymentsList from "../../objects/payments";
 import axios from "axios";
 import { format, parseISO } from 'date-fns';
@@ -15,76 +13,99 @@ function DoctorOverviewPage() {
   let amount = 0
   paymentsList.forEach((element) => element.status === "Pago" ? amount += element.value : amount += 0);
   const [appointments, setAppointments] = useState([])
+  const [patients, setPatients] = useState([]);
 
-    const formatDate = (date) => {
-        return format(parseISO(date), "dd MMM yyyy ' - ' HH:mm");
+  const filterPatient = (patients) => {
+    const conjunto = new Set(patients.map(JSON.stringify));
+    const vetorSemDuplicatas = Array.from(conjunto).map(JSON.parse);
+    return vetorSemDuplicatas;
+  }
+
+  const createPatient = (newData) => {
+    return {
+      id: newData.patient.id_user,
+      name: newData.patient.first_name + " " + newData.patient.last_name,
+      photo: "/images/user.jpeg",
+      phone: newData.patient.phone,
+      age: 22,
+      height: 170,
+      weight: 65,
+      email: newData.patient.email
     }
+  }
 
-    const appointmentStatus = (status) => {
-        if (status === 1) return "Confirmada";
-        else if (status === 2) return "Finalizada";
-        else return "Cancelada"
-    }
+  const formatDate = (date) => {
+    return format(parseISO(date), "dd MMM yyyy ' - ' HH:mm");
+  }
 
-    const verifyToday = (date) => {
-      const today = new Date();
-      const dateToVerify = new Date(date);
-      return today.getFullYear() === dateToVerify.getFullYear() &&
-             today.getMonth() === dateToVerify.getMonth() &&
-             today.getDate() === dateToVerify.getDate();
-    }
+  const appointmentStatus = (status) => {
+    if (status === 1) return "Confirmada";
+    else if (status === 2) return "Finalizada";
+    else return "Cancelada"
+  }
 
-    const verifyAppointment = (newAppointment) => {
-        for (const element of appointments) {
-          if (element.patientid !== newAppointment.patientid) return true
-          if (element.timestamp !== newAppointment.timestamp) return true
-        }
-        return false;
-    }
+  const verifyToday = (date) => {
+    const today = new Date();
+    const dateToVerify = new Date(date);
+    return today.getFullYear() === dateToVerify.getFullYear() &&
+            today.getMonth() === dateToVerify.getMonth() &&
+            today.getDate() === dateToVerify.getDate();
+  }
 
-    const createAppointment = (newAppointment) => {
-        return {
-            id: newAppointment.id,
-            patient: newAppointment.patient.first_name + " " + newAppointment.patient.last_name,
-            patientid: newAppointment.patient.id_user,
-            phone: newAppointment.patient.phone,
-            photo: "/images/user.jpeg",
-            professional: user.name,
-            professionalid: newAppointment.id_user_professional,
-            date: "",
-            hour: "",
-            timestamp: formatDate(newAppointment.datetime),
-            status: appointmentStatus(newAppointment.status),
-            type: "Presencial",
-            address: user.address
-        }
-    }
+  const verifyAppointment = (newAppointment) => {
+      for (const element of appointments) {
+        if (element.patientid !== newAppointment.patientid) return true
+        if (element.timestamp !== newAppointment.timestamp) return true
+      }
+      return false;
+  }
 
-    useEffect(() => {
-        axios.get(`http://localhost:8000/appointment_from_professional_id/${user.id}`)
-            .then(response => {
-              for (const element of response.data) {
-                  const newAppointment = createAppointment(element);
-                  if (verifyToday(element.datetime) && newAppointment.status === "Confirmada") {
-                    if (appointments.length === 0) setAppointments([newAppointment])
-                    if (verifyAppointment(newAppointment)) {
-                        setAppointments(prev => [...prev, newAppointment]);
-                    }
+  const createAppointment = (newAppointment) => {
+      return {
+        id: newAppointment.id,
+        patient: newAppointment.patient.first_name + " " + newAppointment.patient.last_name,
+        patientid: newAppointment.patient.id_user,
+        phone: newAppointment.patient.phone,
+        photo: "/images/user.jpeg",
+        professional: user.name,
+        professionalid: newAppointment.id_user_professional,
+        date: "",
+        hour: "",
+        timestamp: formatDate(newAppointment.datetime),
+        status: appointmentStatus(newAppointment.status),
+        type: "Presencial",
+        address: user.address
+      }
+  }
+
+  useEffect(() => {
+      axios.get(`http://localhost:8000/appointment_from_professional_id/${user.id}`)
+          .then(response => {
+            for (const element of response.data) {
+              const patientList = response.data.map(element => createPatient(element));
+              const patientFilter = filterPatient(patientList);
+              setPatients(patientFilter);
+              const newAppointment = createAppointment(element);
+              if (verifyToday(element.datetime) && newAppointment.status === "Confirmada") {
+                if (appointments.length === 0) setAppointments([newAppointment])
+                if (verifyAppointment(newAppointment)) {
+                    setAppointments(prev => [...prev, newAppointment]);
                 }
               }
-            })
-            .catch(error => {
-                console.log(error)
-            });
-    }, []);
+            }
+          })
+          .catch(error => {
+              console.log(error)
+          });
+  }, []);
 
 
   return (
     <div className="overview page">
       <div className="bignumbers">
-        <BigNumber number={appointmentList.length} text="Consultas" iconType="activity" to="/doctor-appointments" />
+        <BigNumber number={appointments.length} text="Consultas" iconType="activity" to="/doctor-appointments" />
         <BigNumber
-          number={patientList.length}
+          number={patients.length}
           text="Pacientes"
           iconType="user"
           to="/doctor-patients"
