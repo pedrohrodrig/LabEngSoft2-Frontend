9a29a2f2f47e125f303ca8f00d55265ee5fed47f
VictorASantana <victor.asantana012@gmail.com>
2024-04-17 01:32:48 -0300

    adjusting doctor schedule page

--- frontend/src/doctor/doctorschedulepage/doctorschedulepage.js
+++ frontend/src/doctor/doctorschedulepage/doctorschedulepage.js
@@ -1,7 +1,6 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import Title from "../../components/title/title";
 import EventModal from "../../components/eventmodal/eventmodal";
-import NewEventModal from "../../components/neweventmodal/neweventmodal";
 
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
@@ -13,10 +12,9 @@ import ptBR from "date-fns/locale/pt-BR";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 
-import eventsList from "../../objects/events";
+import axios from "axios";
 
 import "./doctorschedulepage.css";
-import Button from "../../components/button/button";
 
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 
@@ -33,9 +31,8 @@ const localizer = dateFnsLocalizer({
 });
 
 function DoctorSchedulePage() {
-  const [events, setEvents] = useState(eventsList);
+  const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
-  const [addNewEvent, setAddNewEvent] = useState(false);
 
   const eventStyle = (event) => ({
     style: {
@@ -43,22 +40,23 @@ function DoctorSchedulePage() {
     },
   });
 
-  const verifyConflict = (newEvent) => {
-    for (let event of eventsList) {
-      if (
-        (newEvent.start >= event.start && newEvent.start <= event.end) ||
-        (newEvent.end >= event.start && newEvent.end <= event.end) ||
-        (newEvent.start <= event.start && newEvent.end >= event.end)
-      )
-      return true;
-    }
-    return false;
-  }
-
   const moveEvents = (data) => {
     const { start, end } = data;
     const updatedEvents = events.map((event) => {
-      if (event.id === data.event.id) {
+      if (event.appointmentid === data.event.appointmentid) {
+        const updated = createAppointmentFromEvent(
+            {
+                ...event,
+                start: new Date(start),
+                end: new Date(end),
+            });
+        axios.put(`http://localhost:8000/appointment/${event.appointmentid}/edit/`, updated)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
         return {
           ...event,
           start: new Date(start),
@@ -67,7 +65,6 @@ function DoctorSchedulePage() {
       }
       return event;
     });
-
     setEvents(updatedEvents);
   };
 
@@ -79,17 +76,6 @@ function DoctorSchedulePage() {
     setSelectedEvent(null);
   };
 
-  const handleAddEventOpen = () => {
-    setAddNewEvent(true);
-  }
-
-  const handleAddEventClose = () => {
-    setAddNewEvent(false);
-  }
-
-  const handleAddEvent = (newEvent) => {
-    setEvents([...events,{...newEvent, id:events.length + 1, appointmentid: events.length + 1}]);
-  }
 
    const handleDeleteEvent = (eventId) => {
     const updatedEvents = events.filter((event) => event.id !== eventId);
@@ -99,7 +85,15 @@ function DoctorSchedulePage() {
 
   const handleEditEvent = (updatedEvent) => {
     const updatedEvents = events.map((event) => {
-      if(event.id === updatedEvent.id) {
+      if(event.appointmentid === updatedEvent.appointmentid) {
+        const updated = createAppointmentFromEvent(updatedEvent);
+        axios.put(`http://localhost:8000/appointment/${event.appointmentid}/edit/`, updated)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
         return updatedEvent;
       }
       return event;
@@ -109,15 +103,53 @@ function DoctorSchedulePage() {
     setSelectedEvent(null);
   }
 
+    const adjustDate = (date) => {
+      return new Date(date);
+    }
+
+   const createAppointmentFromEvent = (event) => {
+    return {
+        id: event.appointmentid,
+        id_user_professional: event.professionalId,
+        patient: event.patientId,
+        start_datetime: event.start,
+        end_datetime: event.end,
+        is_online: event.type === "Online",
+        cancelled: false
+    }
+  }
+
+  const createEventFromAppointment = (appointment) => {
+    return {
+        id: events.length,
+        title: "Consulta " + appointment.id,
+        appointmentid: appointment.id,
+        professionalId: appointment.id_user_professional,
+        patientId: appointment.patient.id,
+        start: adjustDate(appointment.start_datetime),
+        end: adjustDate(appointment.end_datetime),
+        desc: "Consulta",
+        color: "#CCBAF7",
+        type: appointment.is_online ? 'Online' : 'Presencial',
+    }
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/appointment`)
+        .then(response => {
+            console.log(response.data);
+            const newEvents = response.data.map(element => createEventFromAppointment(element));
+            setEvents(newEvents);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+  }, [])
+
   return (
     <div className="schedule page">
       <Title head="Agenda de Consultas" />
       <div className="content">
-        <Button
-          className="grad outline small"
-          text="Nova consulta"
-          onClick={handleAddEventOpen}
-        />
         <div className="calendar-back">
           <div className="calendar">
             <DragAndDropCalendar
@@ -139,9 +171,6 @@ function DoctorSchedulePage() {
           onDelete={handleDeleteEvent}
           onUpdate={handleEditEvent}
         />
-      )}
-      {addNewEvent && (
-        <NewEventModal onAdd={handleAddEvent} onClose={handleAddEventClose} />
       )}
     </div>
   );
