88b0be5fb0f41b5eb0529de80b046013c735aaed
VictorASantana <victor.asantana012@gmail.com>
2024-04-17 01:33:02 -0300

    creating file input component

--- frontend/src/components/fileinput/fileinput.js
+++ frontend/src/components/fileinput/fileinput.js
@@ -9,6 +9,7 @@ function FileInput({ placeholder, handleFileInput, name }) {
       <h3>{placeholder || "Clique para fazer upload"}</h3>
       <p>Tamanho máximo 100mb</p>
       <input type="file" name={name} onChange={handleFileInput} accept="application/pdf" />
+
     </div>
   );
 }
